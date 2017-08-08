08c25e6e1c6446fa3d4c3e45415fcda192e82c4d
Rajashri Lawande <rajashri.lawande@vidushigoc.com>
2017-08-08 18:53:03 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+//site specific configuration declartion
+define( 'BASE_PATH', 'http://localhost/angularjs-insert-update-delete/');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'codexworld');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+
+$mysqli  = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if (mysqli_connect_errno()) {
+	echo("Failed to connect, the error message is : ". mysqli_connect_error());
+	exit();
+}
